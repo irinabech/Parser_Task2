a5bf64f70146d4e1f8a1f6db26b563a0ff737327
irinabech <50566716+irinabech@users.noreply.github.com>
2020-10-21 20:03:16 +0700

    загрузка и чтение JSON

--- js/js.js
+++ js/js.js
@@ -1,17 +1,6 @@
 window.onload = function(){
     //let form = document.querySelector ('#result-generator');
-    
-    //let fileJson = document.getElementById ("#input__file");
-
-    // загрузка json файла //
-    let clear = document.querySelector ('.btn-clear');
-    let btnLoad = document.querySelector('.form__btn');
-    let newForm = document.querySelector('.newForm');
-    btnLoad.onclick = function(event) {
-        newForm.classList.add('form-result');
-        clear.classList.add('clear-active')
-        //createJsonFiles();
-    }; 
+    let fileJson = document.querySelector("#input__file");
 
     //Проверка кол-ва загруженных файлов
     let inputs = document.querySelectorAll('.input__file');
@@ -27,7 +16,28 @@ window.onload = function(){
         else
         label.querySelector('.input__file-button-text').innerText = labelVal;
         });
-    }); 
+    });
+    // загрузка json файла //
+    let clear = document.querySelector ('.btn-clear');
+    let btnLoad = document.querySelector('.form__btn');
+    let newForm = document.querySelector('.newForm');
+    btnLoad.onclick = function(event) {
+        newForm.classList.add('form-result');
+        clear.classList.add('clear-active')
+        loadJson();
+    }; 
 
+     // чтение json //
+    function loadJson() {
+    let loadFile =fileJson.files[0];
+    let resultForm = new FileReader();
+    resultForm.onload = dataJson;
+    resultForm.readAsText(loadFile);
+
+    function dataJson(e) {
+        let newForm = e.target.result;
+        console.log(newForm)
+    }
+    }
 
 }//onload
